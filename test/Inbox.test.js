2258e045e8c8788d3c937953a3dcff69c884a895
rayrcf <raymund.ricabar@celo.org>
2023-08-16 17:30:10 +0800

    initial commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,37 @@
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+const assert = require('assert');
+const web3 = new Web3(ganache.provider());
+const { abi, evm } = require('../compile');
+
+let accounts;
+let inbox;
+const INITIAL_MESSAGE = 'Hi there!';
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    // use one of those accounts to deploy 
+    // the contract
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
+        .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('Bye there!').send( {from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Bye there!');
+    });
+});
